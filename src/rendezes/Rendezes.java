738f92c8f42211754b73146db4b2dd42c85b828f
Wéber Krisztián (SZF_2021_2023) <weber.krisztian@diak.szamalk-szalezi.hu>
2023-04-18 11:06:23 +0200

    rendezés

---
+++ src/rendezes/Rendezes.java
@@ -0,0 +1,58 @@
+
+package rendezes;
+
+import java.util.Random;
+
+public class Rendezes {
+
+    
+    public static void main(String[] args) {
+        int tomb[] = new int[10];
+        feltolt(tomb);
+        kiir(tomb);
+        rend(tomb, 10);
+        rendKiir(tomb);
+    }
+    
+    static void rend(int array[], int n) {
+        
+        int interval = n / 2;
+        while(interval > 0) {
+            for (int i = interval; i < n; i++) {
+                int temp = array[i];
+                int j = i;
+                while(j >= interval && array[j - interval] > temp) {
+                    array[j] = array[j - interval];
+                    j -= interval;
+                }
+                array[j] = temp;
+            }
+            interval /= 2;
+        }
+    }
+    
+    static void feltolt(int array[]) {
+        Random r = new Random();
+        for(int i = 0; i < array.length; i++) {
+            int rand = r.nextInt(100);
+            array[i] = rand;
+        }
+    }
+    
+    static void kiir(int array[]) {
+        System.out.print("A tömb elemei:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println("");
+    }
+    
+    static void rendKiir(int array[]) {
+        System.out.print("A tömb elemei rendezve:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println("");
+    }
+    
+}
